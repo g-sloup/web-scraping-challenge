213dbef0da1f66556c4e814e9f17eef24c91cb14
Genevieve Sloup <genevievesloup@MacBook-Pro-60.local>
2020-11-11 19:09:11 -0500

    added index.html

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,224 +1,139 @@
-#!/usr/bin/env python
-# coding: utf-8
 
 # # Mission to Mars
 
-# In[1]:
-
-
 from splinter import Browser
 from bs4 import BeautifulSoup
-import requests
 import pandas as pd
-import pymongo
-from flask import Flask, render_template, redirect
+import time
 
 
-# In[2]:
+def init_browser():
+    # @NOTE: Replace the path with your actual path to the chromedriver
+    executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
+    return Browser("chrome", **executable_path, headless=False)
 
-
-get_ipython().system('which chromedriver')
-
-
-# In[3]:
-
-
-# Set up Splinter
-executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
-browser = Browser('chrome', **executable_path, headless=False)
-
+def scrape_info():
+    browser = init_browser()
 
 # ## Nasa Mars News
 
-# In[ ]:
+    # Visit URL of page to be scraped through splinter
+    url = 'https://mars.nasa.gov/news/'
 
+    browser.visit(url)
+    time.sleep(1)
 
-# Visit URL of page to be scraped through splinter
-url = 'https://mars.nasa.gov/news/'
+    # Create html object
+    html = browser.html
 
-browser.visit(url)
+    # Parse with BeautifulSoup
+    soup = BeautifulSoup(html, 'html.parser')
 
+    news_title = soup.find_all('div', class_ = 'content_title')[1].text
+    news_p = soup.find_all('div', class_ = 'article_teaser_body')[0].text
 
-# In[ ]:
+    # ## JPL Mars Space Images - Featured Image
 
 
-# Visit URL of page to be scraped through splinter
-url = 'https://mars.nasa.gov/news/'
+    # Visit URL of page to be scraped through splinter
+    jpl_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
 
-browser.visit(url)
+    browser.visit(jpl_url)
+    time.sleep(1)
 
 
-# In[ ]:
 
+    #Click on the the button go to the full image
+    full_image = browser.find_by_id("full_image")
+    full_image.click()
+    browser.is_element_present_by_text("more info")
+    time.sleep(1)
+    full_size = browser.links.find_by_partial_text("more info")
+    full_size.click()
 
-# Create html object
-html = browser.html
 
-# Parse with BeautifulSoup
-soup = BeautifulSoup(html, 'html.parser')
-print(soup.prettify())
 
+    # Create html object
+    jpl_html = browser.html
 
-# In[ ]:
+    # Parse with BeautifulSoup
+    jpl_soup = BeautifulSoup(jpl_html, 'html.parser')
 
 
-# Scrape and collect the latest News Title and Paragraph Text. 
-# Assign the text to variables that you can reference later.
+    # In[ ]:
 
-# Alternatively: (Title is coming up wrong)
-# news_title = soup.find_all('div', class_ = 'content_title')find('a').text
-# news_p = soup.find('div', class_ = 'article_teaser_body').text
 
-news_title = soup.find_all('div', class_ = 'content_title')[0].text
-news_p = soup.find_all('div', class_ = 'article_teaser_body')[0].text
+    #Retrieve backgroud image url from style tag/scrape url
+    img_url = jpl_soup.find("img", class_ = "main_image")["src"]
+    featured_img_url = "https://www.jpl.nasa.gov" + img_url
+    featured_img_url
 
-print("Title: ", news_title)
-print("Paragraph: ", news_p)
 
+    # ## Mars Facts
 
-# ## JPL Mars Space Images - Featured Image
 
-# In[ ]:
+    #Visit the Mars Facts webpage
+    facts_url = "https://space-facts.com/mars/"
+    browser.visit(facts_url)
+    time.sleep(1)
 
 
-# Visit URL of page to be scraped through splinter
-jpl_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    # Use Pandas to parse the URL
+    tables = pd.read_html(facts_url)
 
-browser.visit(jpl_url)
+    #Use Pandas to scrape the table containing facts about the planet including Diameter, Mass, etc.
+    facts_df = tables[0]
 
+    #Assign column names
+    facts_df.columns = ["Description", "Value"]
 
-# In[ ]:
+    #Formatting
+    facts_df["Description"] = facts_df["Description"].str.replace(":", "")
 
+    #Use Pandas to convert the data to a HTML table string
+    facts_html = facts_df.to_html()
 
-#Click on the the button go to the full image
-full_image = browser.find_by_id("full_image")
-full_image.click()
-browser.is_element_present_by_text("more info", wait_time=5)
-full_size = browser.links.find_by_partial_text("more info")
-full_size.click()
+    # ## Mars Hemispheres
 
+    # Visit url to be scraped through splinter
+    hems_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(hems_url)
+    time.sleep(1)
 
-# In[ ]:
-
-
-# Create html object
-jpl_html = browser.html
-
-# Parse with BeautifulSoup
-jpl_soup = BeautifulSoup(jpl_html, 'html.parser')
-
-
-# In[ ]:
-
-
-#Retrieve backgroud image url from style tag/scrape url
-img_url = jpl_soup.find("img", class_ = "main_image")["src"]
-featured_img_url = "https://www.jpl.nasa.gov" + img_url
-featured_img_url
-
-
-# ## Mars Facts
-
-# In[ ]:
-
-
-#Visit the Mars Facts webpage
-facts_url = "https://space-facts.com/mars/"
-browser.visit(facts_url)
-
-
-# In[ ]:
-
-
-# Use Pandas to parse the URL
-tables = pd.read_html(facts_url)
-tables
-
-
-# In[ ]:
-
-
-#Use Pandas to scrape the table containing facts about the planet including Diameter, Mass, etc.
-facts_df = tables[0]
-
-#Assign column names
-facts_df.columns = ["Description", "Value"]
-
-#Formatting
-facts_df["Description"] = facts_df["Description"].str.replace(":", "")
-
-facts_df
-
-
-# In[ ]:
-
-
-#Use Pandas to convert the data to a HTML table string
-facts_html = facts_df.to_html()
-
-print(facts_html)
-
-
-# ## Mars Hemispheres
-
-# In[9]:
-
-
-# Visit url to be scraped through splinter 
-hems_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
-browser.visit(hems_url)
-
-
-# In[10]:
-
-
-# Create HTML object & Parse with BeautifulSoup
-hems_html = browser.html
-
-hems_soup = BeautifulSoup(hems_html, "html.parser")
-
-
-# In[11]:
-
-
-# Create empty list for saving dictionaries title, url
-hemisphere_image_urls = []
-
-products = hems_soup.find('div', class_='result-list')
-
-hemispheres = products.find_all('div',{'class':'item'})
-
-
-# In[16]:
-
-
-# Loop through to append title and url
-for hems in hemispheres:
-    title = hems.find("h3").text
-    title = title.replace(" Enhanced", "")
-    img_link = hems.find("a")["href"]
-    image_url = "https://astrogeology.usgs.gov/" + img_link 
-    
-    browser.visit(image_url)
+    # Create HTML object & Parse with BeautifulSoup
     hems_html = browser.html
+
     hems_soup = BeautifulSoup(hems_html, "html.parser")
-    downloads = hems_soup.find("div", class_="downloads")
-    image_url = downloads.find("a")["href"]
-    hemisphere_image_urls.append({"title": title, "img_url": image_url})
-    
-hemisphere_image_urls
 
+    # Create empty list for saving dictionaries title, url
+    hemisphere_image_urls = []
 
-# In[19]:
+    products = hems_soup.find('div', class_='result-list')
 
+    hemispheres = products.find_all('div', {'class': 'item'})
 
-for i in range(len(hemisphere_image_urls)):
-    print(hemisphere_image_urls[i]['title'])
-    print(hemisphere_image_urls[i]['img_url'] + '\n')
+    # Loop through to append title and url
+    for hems in hemispheres:
+        title = hems.find("h3").text
+        title = title.replace(" Enhanced", "")
+        img_link = hems.find("a")["href"]
+        image_url = "https://astrogeology.usgs.gov/" + img_link
 
+        browser.visit(image_url)
+        time.sleep(1)
+        hems_html = browser.html
+        hems_soup = BeautifulSoup(hems_html, "html.parser")
+        downloads = hems_soup.find("div", class_="downloads")
+        image_url = downloads.find("a")["href"]
+        hemisphere_image_urls.append({"title": title, "img_url": image_url})
 
-# In[ ]:
+    # Assigning scraped data to a page
 
+    mars_page = {}
+    mars_page["news_title"] = news_title
+    mars_page["news_p"] = news_p
+    mars_page["featured_img_url"] = featured_img_url
+    mars_page["facts_html"] = facts_html
+    mars_page["hemisphere_image_urls"] = hemisphere_image_urls
 
-
-
+    return mars_page
